dc58f6af4817869648d6bdcf8b359eb7aa6624a8
slow3rxq <164730155+slow3rxqs@users.noreply.github.com>
2025-11-05 19:22:59 +0300

    Add files via upload

---
+++ src/app/referans/referansList.js
@@ -0,0 +1,20 @@
+export const referansList = [
+  {
+    name: "Evimi Boyamak İstiyorum",
+    description: "Profesyonel boyama hizmetleriyle evinizi yenileyin.",
+    image: "/boya-fırça.png",
+    link: "https://www.evimiboyatmakistiyorum.net",
+  },
+  {
+    name: "Lamar Lingerie",
+    description: "Yeni modeller ile kendinizi güzelleştirin",
+    image: "/lamarlingerie.jpg",
+    link: "https://lamarlingerie.shop",
+  },
+  {
+    name: "TBE Global",
+    description: "Modern ve şık tasarımlar ile evinizi modernleştirin.",
+    image: "/tbeglobal.jpg",
+    link: "https://tbeglobal.net",
+  },
+];
